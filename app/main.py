6f2d087c4f6d15d77dab88c98dc91e3efbcbc43b
abhi <aasusgod@gmail.com>
2024-08-11 14:27:04 +0530

    refactor: Simplify get_posts function by using a predefined list of posts

--- app/main.py
+++ app/main.py
@@ -41,9 +41,6 @@ my_posts = [{
     "id":2
 }]
 
-# @app.delete("/post/{id}")
-# def delete_post(id)
-
 
 def find_post(id):
     for p in my_posts:
@@ -55,27 +52,18 @@ def find_post(id):
 def root():
     return {"message": "Hello World"}
 
-# order of the decorater matters if we have used some variable
-# with same http method like in this case of @app.get("/posts/{id}") 
-# and @app.get("/post/latest") otherwise it will give error by 
-# treating the latest as and id varible
 
 @app.get("/posts/latest")
 def latest_post():
     post = my_posts[len(my_posts)-1]
     return {"latest post": post}
 
- #post id should be converted into integer as the usrl
- #prams always comes as a string 
- 
 @app.get("/posts/{id}")
 def get_post(id : int, response: Response):    
     post = find_post(id)    
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id:{id} was not found")
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"message": f"post with id:{id} was not found"}
     print(post)
     return {"post details": post}
     
@@ -94,15 +82,8 @@ def create_posts( post : Post):
     my_posts.append(post_dic)
     
     print(post)
-    print(post.model_dump()) # equals to post.dict() to print post body in dictionary formate
+    print(post.model_dump()) 
     
     return {"data": post_dic}
-
-
-# from fastapi.params import Body
-# @app.post("/createposts")
-# def create_posts(payload: dict = Body(...)):
-#     print(payload)
-#     return {"newpost": f"title {payload['title']} content {payload['content']}"}
 
 
